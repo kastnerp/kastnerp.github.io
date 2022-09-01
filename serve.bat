4cc3b70c28f013db3224fb31afba3474c0f75fcd
Patrick Kastner <patrick.kastner@gmail.com>
2022-09-01 13:53:30 -0400

    Thesis Announcement

--- serve.bat
+++ serve.bat
@@ -3,7 +3,8 @@ FOR /f "tokens=*" %%i IN ('docker ps -q') DO docker stop %%i
 REM FOR /f "tokens=*" %i IN ('docker ps -q') DO docker stop %i
 REM docker run --rm -v=%cd%:/srv/jekyll -p 4000:4000 -it jekyll/jekyll /bin/bash 
 REM jekyll serve --force_polling --watch
-
 REM docker run --rm -e "JEKYLL_ENV=docker" -v %cd%:/srv/jekyll -p 4000:4000 -it jekyll/jekyll jekyll serve --config  _config.yml,_config.docker.yml
-docker run --rm --volume="%cd%:/srv/jekyll" -p 4000:4000  -it dgraziotin/jekyll-imagemagick6:stable  jekyll serve
+REM docker run --rm --volume="%cd%:/srv/jekyll" -p 4000:4000  -it dgraziotin/jekyll-imagemagick6:stable  jekyll serve
+
+docker run --rm -v "%cd%:/srv/jekyll/" -p "8080:8080"  -it al-folio:latest bundler exec jekyll serve --watch --port=8080 --host=0.0.0.0 
 PAUSE
