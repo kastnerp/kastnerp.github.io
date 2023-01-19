105454f13ff488add8d63aa52cf63e23d20e62bf
Patrick Kastner <patrick.kastner@gmail.com>
2023-01-19 15:27:50 -0500

    Fix docker on Windows
    Update papers.bib

--- serve.bat
+++ serve.bat
@@ -1,10 +1,5 @@
-REM docker run --rm --volume=%cd%:/srv/jekyll -p 4000:4000  jekyll/jekyll jekyll serve --watch --incremental --force_polling
+REM Delete all running containers
 FOR /f "tokens=*" %%i IN ('docker ps -q') DO docker stop %%i
-REM FOR /f "tokens=*" %i IN ('docker ps -q') DO docker stop %i
-REM docker run --rm -v=%cd%:/srv/jekyll -p 4000:4000 -it jekyll/jekyll /bin/bash 
-REM jekyll serve --force_polling --watch
-REM docker run --rm -e "JEKYLL_ENV=docker" -v %cd%:/srv/jekyll -p 4000:4000 -it jekyll/jekyll jekyll serve --config  _config.yml,_config.docker.yml
-REM docker run --rm --volume="%cd%:/srv/jekyll" -p 4000:4000  -it dgraziotin/jekyll-imagemagick6:stable  jekyll serve
-
-docker run --rm -v "%cd%:/srv/jekyll/" -p "8080:8080"  -it al-folio:latest bundler exec jekyll serve --watch --force_polling --port=8080 --host=0.0.0.0 
+REM Serve jekyll in directory
+docker run --rm -v "%cd%:/srv/jekyll/" -p "8080:8080"  -it amirpourmand/al-folio:latest bundler install jekyll serve --watch --force_polling --port=8080 --host=0.0.0.0 
 PAUSE
