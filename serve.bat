3f6bb375bbeaec837a1e721de538a1b784026524
kastnerp <patrick.kastner@gmail.com>
2024-11-13 03:41:17 +0000

    Deploying to gh-pages from @ kastnerp/kastnerp.github.io@8f62db19bc04b24b2da59ddfb9d723a47f5c8b8c 🚀

--- serve.bat
+++ serve.bat
@@ -2,6 +2,6 @@ REM Stop all running Docker containers
 FOR /f "tokens=*" %%i IN ('docker ps -q') DO docker stop %%i
 
 REM Serve Jekyll with bundler exec to resolve gem conflicts
-docker run --rm -v "%cd%:/srv/jekyll" -p "8080:8080" -p "35729:35729" -it amirpourmand/al-folio:latest /bin/sh -c "bundle update && bundle exec jekyll serve --host 0.0.0.0 --port 8080 --watch --livereload --force_polling"
+docker run --rm -v "%cd%:/srv/jekyll" -p "8080:8080" -p "35729:35729" -it amirpourmand/al-folio:v0.12.1 /bin/sh -c "bundle update && bundle exec jekyll serve --host 0.0.0.0 --port 8080 --watch --livereload --force_polling"
 
 PAUSE
