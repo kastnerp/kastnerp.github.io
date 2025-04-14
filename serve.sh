18d1b69b8ced506e7fe4e2e4831a05f6d7fb1c0f
Patrick Kastner <1919773+kastnerp@users.noreply.github.com>
2025-04-14 11:03:12 -0400

    Update to latest version

--- serve.sh
+++ serve.sh
@@ -1,1 +1,1 @@
-docker run --rm -v "$(pwd):/srv/jekyll" -p "8080:8080" -it amirpourmand/al-folio:v0.12.1 /bin/sh -c "bundle update && bundle exec jekyll serve --host 0.0.0.0 --port 8080 --watch --livereload --force_polling"
+docker run --rm -v "$(pwd):/srv/jekyll" -p "8080:8080" -it amirpourmand/al-folio /bin/sh -c "bundle update && bundle exec jekyll serve --host 0.0.0.0 --port 8080 --watch --livereload --force_polling"
